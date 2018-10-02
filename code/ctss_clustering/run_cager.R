fafe12044dec59b3fe3c00736d4aa741e82fc223
Philipp Ross <philippzross@gmail.com>
2018-10-01 23:09:40 -0500

    finished dynamic tsss

--- code/ctss_clustering/run_cager.R
+++ code/ctss_clustering/run_cager.R
@@ -1,4 +1,4 @@
-setwd("/Users/philippross/Dropbox/projects/sanger_rnaseq/")
+setwd("")
 source("code/utils.R")
 
 load_essentials()
@@ -26,25 +26,25 @@ cageset <- new("CAGEset",
 getCTSS(cageset)
 
 # change to outdir
-#setwd(outdir)
+setwd(outdir)
 
 # plot raw correlation values
-#plotCorrelation(cageset, what = "CTSS", samples = "all", method = "pearson")
+plotCorrelation(cageset, what = "CTSS", samples = "all", method = "pearson")
 
 # plot reverse cumulative distributions per sample to estimate parameters to properly normalize
-#plotReverseCumulatives(cageset, values = "raw", onePlot = TRUE)
+plotReverseCumulatives(cageset, values = "raw", onePlot = TRUE)
 
 # normalize tag counts using the power law method
 normalizeTagCount(cageset, method = "powerLaw",
                   fitInRange = c(10, 1000), alpha = 1.28, T = 1e5)
 
 # plot reverse cumulative distributions per sample to see changes after normalization
-#plotReverseCumulatives(cageset,
-#                       values = "normalized", onePlot = TRUE)
+plotReverseCumulatives(cageset,
+                      values = "normalized", onePlot = TRUE)
 
 # export the normalized counts to a bedgraph file
-#exportCTSStoBedGraph(cageset,
-#                     values = "normalized", oneFile = F)
+exportCTSStoBedGraph(cageset,
+                    values = "normalized", oneFile = F)
 
 # cluster TSSs into tag clusters (TCs)
 clusterCTSS(object = cageset,
@@ -64,12 +64,12 @@ quantilePositions(cageset, clusters = "tagClusters",
                   qLow = 0.1, qUp = 0.9)
 
 # plot TC width
-#plotInterquantileWidth(cageset, clusters = "tagClusters",
-#                       tpmThreshold = 1, qLow = 0.1, qUp = 0.9)
+plotInterquantileWidth(cageset, clusters = "tagClusters",
+                      tpmThreshold = 1, qLow = 0.1, qUp = 0.9)
 
 # export to BED file
-#exportToBed(cageset, what = "tagClusters",
-#            qLow = 0.1, qUp = 0.9, oneFile = F)
+exportToBed(cageset, what = "tagClusters",
+           qLow = 0.1, qUp = 0.9, oneFile = F)
 
 # individual tag clusters
 tc1 <- tagClusters(cageset, sample = "tp1",
@@ -88,15 +88,15 @@ tc7 <- tagClusters(cageset, sample = "tp7",
                    returnInterquantileWidth = TRUE, qLow = 0.1, qUp = 0.9)
 
 # export tag clusters
-#export.gff3(GRanges(tc1), "tc1.gff")
-#export.gff3(GRanges(tc2), "tc2.gff")
-#export.gff3(GRanges(tc3), "tc3.gff")
-#export.gff3(GRanges(tc4), "tc4.gff")
-#export.gff3(GRanges(tc5), "tc5.gff")
-#export.gff3(GRanges(tc6), "tc5.gff")
-#export.gff3(GRanges(tc5), "tc5.gff")
-#export.gff3(GRanges(tc6), "tc6.gff")
-#export.gff3(GRanges(tc7), "tc7.gff")
+export.gff3(GRanges(tc1), "tc1.gff")
+export.gff3(GRanges(tc2), "tc2.gff")
+export.gff3(GRanges(tc3), "tc3.gff")
+export.gff3(GRanges(tc4), "tc4.gff")
+export.gff3(GRanges(tc5), "tc5.gff")
+export.gff3(GRanges(tc6), "tc5.gff")
+export.gff3(GRanges(tc5), "tc5.gff")
+export.gff3(GRanges(tc6), "tc6.gff")
+export.gff3(GRanges(tc7), "tc7.gff")
 
 # create consensus promoter clusters (PCs)
 aggregateTagClusters(cageset,
@@ -126,8 +126,8 @@ getExpressionProfiles(cageset, what = "CTSS",
 #plotExpressionProfiles(cageset, what = "CTSS")
 
 # export to BED file with colors
-#exportToBed(cageset, what = "consensusClusters", colorByExpressionProfile = TRUE)
-#exportToBed(cageset, what = "CTSS", colorByExpressionProfile = TRUE)
+exportToBed(cageset, what = "consensusClusters", colorByExpressionProfile = TRUE)
+exportToBed(cageset, what = "CTSS", colorByExpressionProfile = TRUE)
 
 # retrieve consensus clusters
 cc <- consensusClusters(cageset)
@@ -141,133 +141,133 @@ cc$expression.cluster <- apply(cc, 1, function(x) {
   }
 })
 
-#export.gff(cc, "promoter_clusters.gff")
+export.gff(cc, "promoter_clusters.gff")
 
 #-------------------------------------------------------------------------
 # Timecourse
 #-------------------------------------------------------------------------
 
-# # Compare 1 to 2
-# scoreShift(cageset, groupX = "tp1", groupY = "tp2", testKS = TRUE, useTpmKS = TRUE)
-# shifting.promoters12 <- getShiftingPromoters(cageset, tpmThreshold = 5, fdrThreshold = 0.05)
-# write_tsv(shifting.promoters12, "shifting_promoters12.tsv")
-#
-# # Compare 2 to 3
-# scoreShift(cageset, groupX = "tp2", groupY = "tp3", testKS = TRUE, useTpmKS = TRUE)
-# shifting.promoters23 <- getShiftingPromoters(cageset, tpmThreshold = 5, fdrThreshold = 0.05)
-# write_tsv(shifting.promoters23, "shifting_promoters23.tsv")
-#
-# # Compare 3 to 4
-# scoreShift(cageset, groupX = "tp3", groupY = "tp4", testKS = TRUE, useTpmKS = TRUE)
-# shifting.promoters34 <- getShiftingPromoters(cageset, tpmThreshold = 5, fdrThreshold = 0.05)
-# write_tsv(shifting.promoters34, "shifting_promoters34.tsv")
-#
-# # Compare 4 to 5
-# scoreShift(cageset, groupX = "tp4", groupY = "tp5", testKS = TRUE, useTpmKS = TRUE)
-# shifting.promoters45 <- getShiftingPromoters(cageset, tpmThreshold = 5, fdrThreshold = 0.05)
-# write_tsv(shifting.promoters45, "shifting_promoters45.tsv")
-#
-# # Compare 5 to 6
-# scoreShift(cageset, groupX = "tp5", groupY = "tp6", testKS = TRUE, useTpmKS = TRUE)
-# shifting.promoters56 <- getShiftingPromoters(cageset, tpmThreshold = 5, fdrThreshold = 0.05)
-# write_tsv(shifting.promoters56, "shifting_promoters56.tsv")
-#
-# # Compare 6 to 7
-# scoreShift(cageset, groupX = "tp6", groupY = "tp7", testKS = TRUE, useTpmKS = TRUE)
-# shifting.promoters67 <- getShiftingPromoters(cageset, tpmThreshold = 5, fdrThreshold = 0.05)
-# write_tsv(shifting.promoters67, "shifting_promoters67.tsv")
-#
-# #-------------------------------------------------------------------------
-# # Timepoint 1
-# #-------------------------------------------------------------------------
-#
-# # Compare 1 to 3
-# scoreShift(cageset, groupX = "tp1", groupY = "tp3", testKS = TRUE, useTpmKS = TRUE)
-# shifting.promoters13 <- getShiftingPromoters(cageset, tpmThreshold = 5, fdrThreshold = 0.05)
-# write_tsv(shifting.promoters13, "shifting_promoters13.tsv")
-#
-# # Compare 1 to 4
-# scoreShift(cageset, groupX = "tp1", groupY = "tp4", testKS = TRUE, useTpmKS = TRUE)
-# shifting.promoters14 <- getShiftingPromoters(cageset, tpmThreshold = 5, fdrThreshold = 0.05)
-# write_tsv(shifting.promoters14, "shifting_promoters14.tsv")
-#
-# # Compare 1 to 5
-# scoreShift(cageset, groupX = "tp1", groupY = "tp5", testKS = TRUE, useTpmKS = TRUE)
-# shifting.promoters15 <- getShiftingPromoters(cageset, tpmThreshold = 5, fdrThreshold = 0.05)
-# write_tsv(shifting.promoters15, "shifting_promoters15.tsv")
-#
-# # Compare 1 to 6
-# scoreShift(cageset, groupX = "tp1", groupY = "tp6", testKS = TRUE, useTpmKS = TRUE)
-# shifting.promoters16 <- getShiftingPromoters(cageset, tpmThreshold = 5, fdrThreshold = 0.05)
-# write_tsv(shifting.promoters16, "shifting_promoters16.tsv")
-#
-# # Compare 1 to 7
-# scoreShift(cageset, groupX = "tp1", groupY = "tp7", testKS = TRUE, useTpmKS = TRUE)
-# shifting.promoters17 <- getShiftingPromoters(cageset, tpmThreshold = 5, fdrThreshold = 0.05)
-# write_tsv(shifting.promoters17, "shifting_promoters17.tsv")
-#
-# #-------------------------------------------------------------------------
-# # Timepoint 2
-# #-------------------------------------------------------------------------
-#
-# # Compare 2 to 4
-# scoreShift(cageset, groupX = "tp2", groupY = "tp4", testKS = TRUE, useTpmKS = TRUE)
-# shifting.promoters24 <- getShiftingPromoters(cageset, tpmThreshold = 5, fdrThreshold = 0.05)
-# write_tsv(shifting.promoters24, "shifting_promoters24.tsv")
-#
-# # Compare 2 to 5
-# scoreShift(cageset, groupX = "tp2", groupY = "tp5", testKS = TRUE, useTpmKS = TRUE)
-# shifting.promoters25 <- getShiftingPromoters(cageset, tpmThreshold = 5, fdrThreshold = 0.05)
-# write_tsv(shifting.promoters25, "shifting_promoters25.tsv")
-#
-# # Compare 2 to 6
-# scoreShift(cageset, groupX = "tp2", groupY = "tp6", testKS = TRUE, useTpmKS = TRUE)
-# shifting.promoters26 <- getShiftingPromoters(cageset, tpmThreshold = 5, fdrThreshold = 0.05)
-# write_tsv(shifting.promoters26, "shifting_promoters26.tsv")
-#
-# # Compare 2 to 7
-# scoreShift(cageset, groupX = "tp2", groupY = "tp7", testKS = TRUE, useTpmKS = TRUE)
-# shifting.promoters27 <- getShiftingPromoters(cageset, tpmThreshold = 5, fdrThreshold = 0.05)
-# write_tsv(shifting.promoters27, "shifting_promoters27.tsv")
-#
-# #-------------------------------------------------------------------------
-# # Timepoint 3
-# #-------------------------------------------------------------------------
-#
-# # Compare 3 to 5
-# scoreShift(cageset, groupX = "tp3", groupY = "tp5", testKS = TRUE, useTpmKS = TRUE)
-# shifting.promoters35 <- getShiftingPromoters(cageset, tpmThreshold = 5, fdrThreshold = 0.05)
-# write_tsv(shifting.promoters35, "shifting_promoters35.tsv")
-#
-# # Compare 3 to 6
-# scoreShift(cageset, groupX = "tp3", groupY = "tp6", testKS = TRUE, useTpmKS = TRUE)
-# shifting.promoters36 <- getShiftingPromoters(cageset, tpmThreshold = 5, fdrThreshold = 0.05)
-# write_tsv(shifting.promoters36, "shifting_promoters36.tsv")
-#
-# # Compare 3 to 7
-# scoreShift(cageset, groupX = "tp3", groupY = "tp7", testKS = TRUE, useTpmKS = TRUE)
-# shifting.promoters37 <- getShiftingPromoters(cageset, tpmThreshold = 5, fdrThreshold = 0.05)
-# write_tsv(shifting.promoters37, "shifting_promoters37.tsv")
-#
-# #-------------------------------------------------------------------------
-# # Timepoint 4
-# #-------------------------------------------------------------------------
-#
-# # Compare 4 to 6
-# scoreShift(cageset, groupX = "tp4", groupY = "tp6", testKS = TRUE, useTpmKS = TRUE)
-# shifting.promoters46 <- getShiftingPromoters(cageset, tpmThreshold = 5, fdrThreshold = 0.05)
-# write_tsv(shifting.promoters46, "shifting_promoters46.tsv")
-#
-# # Compare 4 to 7
-# scoreShift(cageset, groupX = "tp4", groupY = "tp7", testKS = TRUE, useTpmKS = TRUE)
-# shifting.promoters47 <- getShiftingPromoters(cageset, tpmThreshold = 5, fdrThreshold = 0.05)
-# write_tsv(shifting.promoters47, "shifting_promoters47.tsv")
-#
-# #-------------------------------------------------------------------------
-# # Timepoint 5
-# #-------------------------------------------------------------------------
-#
-# # Compare 5 to 7
-# scoreShift(cageset, groupX = "tp4", groupY = "tp7", testKS = TRUE, useTpmKS = TRUE)
-# shifting.promoters57 <- getShiftingPromoters(cageset, tpmThreshold = 5, fdrThreshold = 0.05)
-# write_tsv(shifting.promoters57, "shifting_promoters57.tsv")
+# Compare 1 to 2
+scoreShift(cageset, groupX = "tp1", groupY = "tp2", testKS = TRUE, useTpmKS = TRUE)
+shifting.promoters12 <- getShiftingPromoters(cageset, tpmThreshold = 5, fdrThreshold = 0.05)
+write_tsv(shifting.promoters12, "shifting_promoters12.tsv")
+
+# Compare 2 to 3
+scoreShift(cageset, groupX = "tp2", groupY = "tp3", testKS = TRUE, useTpmKS = TRUE)
+shifting.promoters23 <- getShiftingPromoters(cageset, tpmThreshold = 5, fdrThreshold = 0.05)
+write_tsv(shifting.promoters23, "shifting_promoters23.tsv")
+
+# Compare 3 to 4
+scoreShift(cageset, groupX = "tp3", groupY = "tp4", testKS = TRUE, useTpmKS = TRUE)
+shifting.promoters34 <- getShiftingPromoters(cageset, tpmThreshold = 5, fdrThreshold = 0.05)
+write_tsv(shifting.promoters34, "shifting_promoters34.tsv")
+
+# Compare 4 to 5
+scoreShift(cageset, groupX = "tp4", groupY = "tp5", testKS = TRUE, useTpmKS = TRUE)
+shifting.promoters45 <- getShiftingPromoters(cageset, tpmThreshold = 5, fdrThreshold = 0.05)
+write_tsv(shifting.promoters45, "shifting_promoters45.tsv")
+
+# Compare 5 to 6
+scoreShift(cageset, groupX = "tp5", groupY = "tp6", testKS = TRUE, useTpmKS = TRUE)
+shifting.promoters56 <- getShiftingPromoters(cageset, tpmThreshold = 5, fdrThreshold = 0.05)
+write_tsv(shifting.promoters56, "shifting_promoters56.tsv")
+
+# Compare 6 to 7
+scoreShift(cageset, groupX = "tp6", groupY = "tp7", testKS = TRUE, useTpmKS = TRUE)
+shifting.promoters67 <- getShiftingPromoters(cageset, tpmThreshold = 5, fdrThreshold = 0.05)
+write_tsv(shifting.promoters67, "shifting_promoters67.tsv")
+
+#-------------------------------------------------------------------------
+# Timepoint 1
+#-------------------------------------------------------------------------
+
+# Compare 1 to 3
+scoreShift(cageset, groupX = "tp1", groupY = "tp3", testKS = TRUE, useTpmKS = TRUE)
+shifting.promoters13 <- getShiftingPromoters(cageset, tpmThreshold = 5, fdrThreshold = 0.05)
+write_tsv(shifting.promoters13, "shifting_promoters13.tsv")
+
+# Compare 1 to 4
+scoreShift(cageset, groupX = "tp1", groupY = "tp4", testKS = TRUE, useTpmKS = TRUE)
+shifting.promoters14 <- getShiftingPromoters(cageset, tpmThreshold = 5, fdrThreshold = 0.05)
+write_tsv(shifting.promoters14, "shifting_promoters14.tsv")
+
+# Compare 1 to 5
+scoreShift(cageset, groupX = "tp1", groupY = "tp5", testKS = TRUE, useTpmKS = TRUE)
+shifting.promoters15 <- getShiftingPromoters(cageset, tpmThreshold = 5, fdrThreshold = 0.05)
+write_tsv(shifting.promoters15, "shifting_promoters15.tsv")
+
+# Compare 1 to 6
+scoreShift(cageset, groupX = "tp1", groupY = "tp6", testKS = TRUE, useTpmKS = TRUE)
+shifting.promoters16 <- getShiftingPromoters(cageset, tpmThreshold = 5, fdrThreshold = 0.05)
+write_tsv(shifting.promoters16, "shifting_promoters16.tsv")
+
+# Compare 1 to 7
+scoreShift(cageset, groupX = "tp1", groupY = "tp7", testKS = TRUE, useTpmKS = TRUE)
+shifting.promoters17 <- getShiftingPromoters(cageset, tpmThreshold = 5, fdrThreshold = 0.05)
+write_tsv(shifting.promoters17, "shifting_promoters17.tsv")
+
+#-------------------------------------------------------------------------
+# Timepoint 2
+#-------------------------------------------------------------------------
+
+# Compare 2 to 4
+scoreShift(cageset, groupX = "tp2", groupY = "tp4", testKS = TRUE, useTpmKS = TRUE)
+shifting.promoters24 <- getShiftingPromoters(cageset, tpmThreshold = 5, fdrThreshold = 0.05)
+write_tsv(shifting.promoters24, "shifting_promoters24.tsv")
+
+# Compare 2 to 5
+scoreShift(cageset, groupX = "tp2", groupY = "tp5", testKS = TRUE, useTpmKS = TRUE)
+shifting.promoters25 <- getShiftingPromoters(cageset, tpmThreshold = 5, fdrThreshold = 0.05)
+write_tsv(shifting.promoters25, "shifting_promoters25.tsv")
+
+# Compare 2 to 6
+scoreShift(cageset, groupX = "tp2", groupY = "tp6", testKS = TRUE, useTpmKS = TRUE)
+shifting.promoters26 <- getShiftingPromoters(cageset, tpmThreshold = 5, fdrThreshold = 0.05)
+write_tsv(shifting.promoters26, "shifting_promoters26.tsv")
+
+# Compare 2 to 7
+scoreShift(cageset, groupX = "tp2", groupY = "tp7", testKS = TRUE, useTpmKS = TRUE)
+shifting.promoters27 <- getShiftingPromoters(cageset, tpmThreshold = 5, fdrThreshold = 0.05)
+write_tsv(shifting.promoters27, "shifting_promoters27.tsv")
+
+#-------------------------------------------------------------------------
+# Timepoint 3
+#-------------------------------------------------------------------------
+
+# Compare 3 to 5
+scoreShift(cageset, groupX = "tp3", groupY = "tp5", testKS = TRUE, useTpmKS = TRUE)
+shifting.promoters35 <- getShiftingPromoters(cageset, tpmThreshold = 5, fdrThreshold = 0.05)
+write_tsv(shifting.promoters35, "shifting_promoters35.tsv")
+
+# Compare 3 to 6
+scoreShift(cageset, groupX = "tp3", groupY = "tp6", testKS = TRUE, useTpmKS = TRUE)
+shifting.promoters36 <- getShiftingPromoters(cageset, tpmThreshold = 5, fdrThreshold = 0.05)
+write_tsv(shifting.promoters36, "shifting_promoters36.tsv")
+
+# Compare 3 to 7
+scoreShift(cageset, groupX = "tp3", groupY = "tp7", testKS = TRUE, useTpmKS = TRUE)
+shifting.promoters37 <- getShiftingPromoters(cageset, tpmThreshold = 5, fdrThreshold = 0.05)
+write_tsv(shifting.promoters37, "shifting_promoters37.tsv")
+
+#-------------------------------------------------------------------------
+# Timepoint 4
+#-------------------------------------------------------------------------
+
+# Compare 4 to 6
+scoreShift(cageset, groupX = "tp4", groupY = "tp6", testKS = TRUE, useTpmKS = TRUE)
+shifting.promoters46 <- getShiftingPromoters(cageset, tpmThreshold = 5, fdrThreshold = 0.05)
+write_tsv(shifting.promoters46, "shifting_promoters46.tsv")
+
+# Compare 4 to 7
+scoreShift(cageset, groupX = "tp4", groupY = "tp7", testKS = TRUE, useTpmKS = TRUE)
+shifting.promoters47 <- getShiftingPromoters(cageset, tpmThreshold = 5, fdrThreshold = 0.05)
+write_tsv(shifting.promoters47, "shifting_promoters47.tsv")
+
+#-------------------------------------------------------------------------
+# Timepoint 5
+#-------------------------------------------------------------------------
+
+# Compare 5 to 7
+scoreShift(cageset, groupX = "tp4", groupY = "tp7", testKS = TRUE, useTpmKS = TRUE)
+shifting.promoters57 <- getShiftingPromoters(cageset, tpmThreshold = 5, fdrThreshold = 0.05)
+write_tsv(shifting.promoters57, "shifting_promoters57.tsv")
